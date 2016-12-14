b301f286841b2f4a0b6d09d0f71e323ac6c25c0e
Michael Irby <michael.irbstoomuch@gmail.com>
2016-12-14 17:45:26 -0500

    JS updates and JQ revisions

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,99 @@
 // GLOBAL VARIABLES
-var theDeck = createDeck();
+const freshDeck = createDeck();
+var theDeck = freshDeck;
 var playersHand = [];
 var dealersHand = [];
+var hitCounter = 0;
 
 $(document).ready(function(){
 
 	$('.deal-button').click(function(){
+		$('.deal-button').attr('disabled', true);
 		shuffleDeck();
-		playersHand.push(theDeck[0]);
-		playersHand.push(theDeck[2]);
+		// Use shift to remove the top card from the deck
+		playersHand.push(theDeck.shift());
+		dealersHand.push(theDeck.shift());
+		playersHand.push(theDeck.shift());
+		dealersHand.push(theDeck.shift());
 
-		placeCard('player', 'one', playersHand[0]);
-		placeCard('player', 'two', playersHand[1]);
+		placeCard('player', 1, playersHand[0]);
+		placeCard('player', 2, playersHand[1]);
+		placeCard('dealer', 1, dealersHand[0]);
+		placeCard('dealer', 2, 'deck');
 
-		dealersHand.push(theDeck[1]);
-		dealersHand.push(theDeck[3]);
-
-		placeCard('dealer', 'one', dealersHand[0]);
-		placeCard('dealer', 'two', dealersHand[1]);
+		calculateTotal(playersHand, 'player');
+		calculateTotal(dealersHand, 'dealer');
+		hitCounter++;
 	});
 
 	$('.hit-button').click(function(){
+		if(calculateTotal(playersHand, 'player') < 21){
+			playersHand.push(theDeck.shift());
+			var slotForNewCard = playersHand.length;
+			placeCard('player', slotForNewCard, playersHand[playersHand.length-1]);
+			calculateTotal(playersHand, 'player');
+		}
 
 	});
 
 	$('.stand-button').click(function(){
-
+		placeCard('dealer', 2, dealersHand[dealersHand.length - 1]);  //Shows card that was hidden
+		var dealerTotal = calculateTotal(dealersHand, 'dealer');
+		while(dealerTotal < 17){
+			// Dealer will always hit while less than 17
+			dealersHand.push(theDeck.shift());
+			var slotForNewCard = dealersHand.length;
+			placeCard('dealer', slotForNewCard, dealersHand[dealersHand.length-1]);
+			dealerTotal = calculateTotal(dealersHand, 'dealer');
+		}
+		// The dealer has 17 or more Player hit stand. Check to see who won.
+		checkWin();
 	});
 
 
 });
+
+function checkWin(){
+	playerTotal = calculateTotal(playersHand, 'player');
+	dealerTotal = calculateTotal(dealersHand, 'dealer');
+
+	// player has more than 21. player busts and loses
+	if(playerTotal > 21){
+		$('.dealer-win').text("Dealer wins with: ");
+	}
+	else if(dealerTotal > 21){
+		$('.player-win').text("Player wins with: ");
+	}
+	else{
+		if(playerTotal > dealerTotal){
+			$('.player-win').text("Player wins with: ");
+		}
+		else if(dealerTotal > playerTotal){
+			$('.dealer-win').text("Dealer wins with: ");
+		}
+		else{
+			$('.player-win').text("It's a tie with: ");
+			$('.dealer-win').text("It's a tie with: ");
+		}
+	}
+}
+
+function reset(){
+	// the deck needs to be reset
+	theDeck = freshDeck; //Make a copy of our constnt freshDeck
+	// the player and dealer hands need to be reset
+	playersHand = [];
+	dealersHand = [];
+	// reset the DOM
+	// -cards
+	$('.card').html('');
+	$('.deck').html('<img src="images/cards/deck.png">');
+	$('.deal-button').attr('disabled', false); //Try and make card back remain after reset!!
+	$('.player-win').text('Player Total: ');
+	$('.dealer-win').text('Dealer Total: ');
+	playerTotal = calculateTotal(playersHand, 'player');
+	dealerTotal = calculateTotal(dealersHand, 'dealer');
+}
 
 function createDeck(){
 	var newDeck = [];
@@ -55,4 +119,48 @@ function shuffleDeck(){
 function placeCard(who, where, whatCard){
 	var classSelector = '.' + who + '-cards .card-' + where;
 	$(classSelector).html('<img src="images/cards/' + whatCard + '.png">');
+	$(classSelector).hide().fadeIn(1000);
+	// for (var i = 0; i < 5; i++){
+	// 	$('.card-move').animate({
+	// 		'margin-top': '+=10px'
+
+	// 	})
+	// }
+	
+
 }
+
+function calculateTotal(hand, who){
+	var total = 0;
+	var cardValue = 0;
+	var numberOfAces = 0;
+	for(let i = 0; i < hand.length; i++){
+		if((hitCounter == 0)&&(who === 'dealer')&&(i === 1)){ //Followed Paul's logic here. Don't fully understand.
+		}
+		else{
+			cardValue = Number(hand[i].slice(0, -1));
+		if(cardValue > 10){
+			cardValue = 10;
+		}
+		if((cardValue === 1)&&(total < 11)){
+			cardValue = 11;
+			numberOfAces++;
+		}
+		total += cardValue;
+		}
+		// if((total > 21)&&(hasAce == true)){
+		// 	total -= 10;
+		// }
+		if((numberOfAces > 0)&&(total > 21)){
+			total -= 10;
+			numberOfAces--;
+		}
+
+		
+	}
+	var classSelector = '.' + who + '-total-number';
+	$(classSelector).text(total);
+	return total;
+}
+
+
